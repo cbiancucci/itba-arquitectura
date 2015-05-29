727ac3218479bd9250771f367844ee9d49d7a905
Christian Biancucci <christian.biancucci@mercadolibre.com>
2015-05-29 16:48:36 -0300

    Add Definitions file for common kernel definitions.

---
+++ Kernel/include/definitions.h
@@ -0,0 +1,20 @@
+/******
+	General Definitions
+******/
+
+#ifndef _defs_
+#define _defs_
+
+#define byte	unsigned char
+#define word	short int
+#define dword	int
+
+#define TRUE	1
+#define FALSE	0
+#define NULL	0
+
+/* File descriptors */
+#define STDIN	0
+#define STDOUT	1
+#define STDERR	2
+
