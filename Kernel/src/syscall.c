b252a2f34b6e0850e952c2428d817919acf43e22
Christian Biancucci <christian.biancucci@mercadolibre.com>
2015-05-29 16:56:42 -0300

    First implementation of system calls.

---
+++ Kernel/src/syscall.c
@@ -0,0 +1,9 @@
+#include "../include/definitions.h"
+
+size_t __read(int fd, byte * buffer, size_t count){
+	// implementacion de syscall read
+}
+
+size_t __write(int fd, const byte * buffer, size_t count){
+	// implementacion de syscall write
+}
