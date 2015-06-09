e9745b062dab9f261bb5fd5e124614ad9bca8763
Christian Biancucci <christian.biancucci@mercadolibre.com>
2015-06-09 16:45:01 -0300

    Code depuration.

--- API/definitions.h
+++ API/definitions.h
@@ -38,20 +38,4 @@ typedef enum {
 /** VIDEO ATTRIBUTES **/
 typedef uint8_t color_t;
 
-/* Text color */
-#define BLUE_TEXT		0x01 // Letras azules, fondo negro.
-#define GREEN_TEXT		0x02 // Letras verdes, fondo negro.
-#define CYAN_TEXT		0x03 // Letras celestes, fondo negro.
-#define RED_TEXT		0x04 // Letras rojas, fondo negro.
-#define PURPLE_TEXT		0x05 // Letras violetas, fondo negro.
-#define YELLOW_TEXT		0x06 // Letras amarillas, fondo negro.
-#define WHITE_TEXT		0x07 // Letras blancas, fondo negro.
-
-/* Buffer */
-#define BUFFER_SIZE		1024
-
-/* Shell prompt */
-#define PROMPT "sarasa$ "
-
-
 #endif
