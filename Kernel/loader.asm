c064e27062330ed2942ecd57617a054d10aedb6e
Christian Biancucci <christian.biancucci@mercadolibre.com>
2015-06-09 18:29:23 -0300

    Code formatter.

--- Kernel/loader.asm
+++ Kernel/loader.asm
@@ -23,7 +23,7 @@ loader:
 
 		call 		initializeKernelBinary		; Set up the kernel binary, and get thet stack address
 
-		mov			rsp, 	rax					; Set up the stack with the returned address
+		mov			rsp, rax					; Set up the stack with the returned address
 		push 		rax
 
 		call 		set_interrupt_handlers
@@ -32,7 +32,7 @@ loader:
 
 
 hang:
-		cli
+		cli										; Clear interrupts
 		hlt										; halt machine should kernel return
 		jmp 		hang
 
@@ -45,12 +45,12 @@ create_gate:
 		push 		rdi
 		push 		rax
 
-		shl 		rdi,	4					; quickly multiply rdi by 16
+		shl 		rdi, 4						; quickly multiply rdi by 16
 		stosw									; store the low word (15..0)
-		shr 		rax, 	16
-		add 		rdi,	4					; skip the gate marker
+		shr 		rax, 16
+		add 		rdi, 4						; skip the gate marker
 		stosw									; store the high word (31..16)
-		shr 		rax,	16
+		shr 		rax, 16
 		stosd									; store the high dword (63..32)
 
 		pop 		rax
@@ -67,41 +67,40 @@ set_interrupt_handlers:
 		mov 		rax, software_interruptions
 		call		create_gate
 
-		mov		rdi, 0x20
-		mov		rax, pit_handler
+		mov			rdi, 0x20
+		mov			rax, pit_handler
 		call		create_gate
 
 		lidt 		[IDTR64]					; load IDT register
-
 		ret
 
 align 16
-software_interruptions:									; Interrupciones de software, int 80h
+software_interruptions:							; Interrupciones de software, int 80h
 		push 		rdi
 		;push 		rax
 
-		cmp			rdi, 	3
+		cmp			rdi, 3
 		jz 			int_sys_read
 
-		cmp			rdi, 	4
+		cmp			rdi, 4
 		jz			int_sys_write
 
-		cmp 		rdi,	5
+		cmp 		rdi, 5
 		jz 			int_malloc
 
-		cmp 		rdi,	6
+		cmp 		rdi, 6
 		jz 			int_calloc
 
-		cmp 		rdi,	7
+		cmp 		rdi, 7
 		jz 			int_free
 
-		cmp 		rdi, 	10
+		cmp 		rdi, 10
 		jz 			int_keyboard_replace_buffer
 
-		cmp			rdi,	17
+		cmp			rdi, 17
 		jz			int_sys_clear_screen
 
-		cmp			rdi,	18
+		cmp			rdi, 18
 		jz			hang
 
 		jmp 		soft_interrupt_done 		; La syscall no existe
@@ -142,18 +141,18 @@ int_sys_clear_screen:
 
 soft_interrupt_done:
 		push 		rax
-		mov 		al, 	0x20				; Acknowledge the IRQ
-		out 		0x20, 	al
+		mov 		al, 0x20				; Acknowledge the IRQ
+		out 		0x20, al
 
 		pop 		rax
 		pop 		rdi
 		iretq
 
 prepare_params:
-		mov 		rdi,	rsi
-		mov 		rsi,	rdx
-		mov 		rdx,	rcx
-		mov			rcx,	r8
+		mov 		rdi, rsi
+		mov 		rsi, rdx
+		mov 		rdx, rcx
+		mov			rcx, r8
 
 		ret
 
@@ -163,27 +162,28 @@ pit_handler:
 
 		call 		irq0_handler
 
-		mov			al, 	0x20				; Acknowledge the IRQ
-		out 		0x20, 	al
+		mov			al, 0x20					; Acknowledge the IRQ
+		out 		0x20, al
 
 		pop 		rax
 		pop 		rdi
 		iretq
+
 align 16
 keyboard:
 		push 		rdi
 		push 		rax
 
-		xor			eax, 	eax
+		xor			eax, eax
 
-		in 			al, 	0x60				; Get the scancode from the keyboard
+		in 			al, 0x60					; Get the scancode from the keyboard
 
-		mov 		rdi,	 rax
+		mov 		rdi, rax
 		call 		keyboardHandler
 
 keyboard_done:
-		mov			al, 	0x20				; Acknowledge the IRQ
-		out 		0x20, 	al
+		mov			al,  0x20					; Acknowledge the IRQ
+		out 		0x20, al
 
 		pop 		rax
 		pop 		rdi
@@ -192,9 +192,8 @@ keyboard_done:
 init_pic:
 	; Enable specific interrupts
 	in al, 0x21
-	mov al, 11111000b		; Enable Cascade, Keyboard
+	mov al, 11111000b							; Enable Cascade, Keyboard
 	out 0x21, al
 
-	sti				; Enable interrupts
-
+	sti											; Enable interrupts
 	ret
